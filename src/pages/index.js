d461079973c75a025661b0a13842fed929cf080a
Bruno <brunoomiguel10@hotmail.com>
2023-07-04 15:34:09 +0100

    Working with local .json storage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import './style.css';
 
 export const Index = (props) => {
+  const data = require("../data.json");
+  console.log(data);
   return (
     <div>
       <h1 className="text-3xl font-bold underline">
-        {props.name}
+        {data[0].name} {props.name}
       </h1>
     </div>
   )
