6e9b4e6d75aa8d3b49c9f3b557662237434d2fea
Bruno <brunoomiguel10@hotmail.com>
2023-07-05 16:56:07 +0100

    Properly working with json data and useState

--- src/components/todo.tsx
+++ src/components/todo.tsx
@@ -1,7 +1,7 @@
 export const Todo = (props:any) => {
   return (
-    <div>
-      
+    <div className="border-solid border-white border-t-2 py-4">
+      <h1 className="text-2xl text-white">Name:{props.name}</h1> 
     </div>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,26 @@
 import "./style.css";
-import "../components/todo";
+import {Todo} from "../components/todo";
+import jsonData from "../data.json";
+import React, { useState } from "react";
 
 export const Index = (props:any) => {
-  const data = require("../data.json");
+  const [data, setData] = useState(jsonData);
   return (
     <div className="h-screen w-screen bg-gradient-to-r from-gray-800 to-gray-700">
-      <h1 className="text-5xl font-bold text-center pt-12 text-white">
+      <h1 className="text-5xl font-bold text-center pt-12 bg-gradient-to-r from-gray-100 to-blue-300 bg-clip-text text-transparent w-2/4 m-auto">
         To Do App
       </h1>
-      <div className="w-3/4 h-3/4 m-auto mt-8 rounded">
-        <p className="text-white">Added new to do: </p>
-        <input className="p-1 rounded border border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent drop-shadow"/>
-        <button className="ml-2 p-1 px-5 rounded bg-purple-200 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50"> Add </button>
+      <div className="w-3/4 m-auto mt-8 rounded">
+        <p className="text-white">Added new task: </p>
+        <input className="p-2 rounded border border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent drop-shadow"/>
+        <button className="ml-2 p-2 px-5 rounded bg-purple-200 hover:bg-purple-700 
+          hover:text-white focus:outline-none focus:ring-2 focus:ring-purple-600 
+          focus:ring-opacity-50"> Add </button>
+      </div>
+      <div className="pt-10 w-3/4 m-auto">
+        {data.map((info:any) => (
+          <Todo name={info.name}/>
+        ))}
       </div>
     </div>
   )
